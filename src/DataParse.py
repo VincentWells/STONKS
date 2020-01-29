d27620a82bfb0592eafe952148c30ac64dd6b3cb
Vincent Wells <vd2w@comcast.net>
2020-01-29 11:55:22 -0500

    untested parsing code added

--- src/DataParse.py
+++ src/DataParse.py
@@ -1,25 +1,19 @@
 # import from DataGather?
 years = range(2009, 2019)
 quarters = ["q1", "q2", "q3", "q4"]
-company_to_adsh = {}
+
 
 # theory: don't need to read all sub.tsv files, just the last one, as long as no company has gone out of business
-with open('../data/2019q3/sub.tsv', 'r') as f:
-    for line in enumerate(f):
-        line_list = line.split('\t')
-        # adsh column
-        adsh = line_list[0]
-        # name column
-        name = line_list[2]
-        company_to_adsh[adsh] = name
 
 # Might be simpler to build SQL database and run query
-adsh_to_tab_value = {}
+all_data = []
+tag_list = []
 for year in years:
     for quarter in quarters:
         dir_addr = '../data/' + str(year) + quarter
-
-        with open(dir_addr + '/num.tsv', 'wb') as f:
+        company_to_adsh = {}
+        with open(dir_addr + '/sub.tsv', 'r') as f:
+            next(f)
             for line in enumerate(f):
                 line_list = line.split('\t')
                 # adsh column
@@ -27,4 +21,25 @@ for year in years:
                 # name column
                 name = line_list[2]
                 company_to_adsh[adsh] = name
+        with open(dir_addr + '/num.tsv', 'r') as f:
+            next(f)
+            quarter_data = {}
+            for line in enumerate(f):
+                line_list = line.split('\t')
+
+                # tag column
+                tag = line_list[1]
+                # value column
+                # TODO: remove decimal points and whatnot from this value
+                val = line_list[8]
+
+                name = company_to_adsh[line_list[0]]
+                if tag in tag_list:
+                    if name in quarter_data:
+                        quarter_data[name] = [0] * len(tag_list)
+                    quarter_data[name][tag_list.index(tag)] = val
+            all_data.append(quarter_data)
+
+
+
 
